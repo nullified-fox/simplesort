865cd106777e0c80aee8ee05fa47cf3bad5bd489
Nicholas <nickjsirois@gmail.com>
2026-05-10 14:05:26 -0700

    Add copyright headers to Java files
    Insert FanaticalFoxx (2026) copyright and usage notice

--- src/client/java/me/simplesort/client/ModMenuIntegration.java
+++ src/client/java/me/simplesort/client/ModMenuIntegration.java
@@ -1,5 +1,18 @@
 package me.simplesort.client;
 
+/**
+    Copyright (c) 2026 FanaticalFoxx. All Rights Reserved.
+
+    You may NOT:
+    - Redistribute, copy, or mirror the source code or compiled files
+    - Modify or create derivative works
+    - Reupload to any mod hosting platform
+
+    You MAY:
+    - Include this mod in modpacks (public or private) without prior permission
+    - Share modpack links that reference the official download
+ */
+
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
--- src/client/java/me/simplesort/client/SortTrailRenderer.java
+++ src/client/java/me/simplesort/client/SortTrailRenderer.java
@@ -1,3 +1,16 @@
+/**
+    Copyright (c) 2026 FanaticalFoxx. All Rights Reserved.
+
+    You may NOT:
+    - Redistribute, copy, or mirror the source code or compiled files
+    - Modify or create derivative works
+    - Reupload to any mod hosting platform
+
+    You MAY:
+    - Include this mod in modpacks (public or private) without prior permission
+    - Share modpack links that reference the official download
+ */
+
 package me.simplesort.client;
 
 import java.util.ArrayList;
